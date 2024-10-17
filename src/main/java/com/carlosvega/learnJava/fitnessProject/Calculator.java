15509695e62df66e0d746176572516285b084fd1
Carlos Vega <carlos_cmvp@hotmail.com>
2024-10-17 16:08:01 -0600

    change method name

--- src/main/java/com/carlosvega/learnJava/fitnessProject/Calculator.java
+++ src/main/java/com/carlosvega/learnJava/fitnessProject/Calculator.java
@@ -2,5 +2,5 @@ package com.carlosvega.learnJava.fitnessProject;
 //crear una interfaz generica es como crear una clase
 public interface Calculator <T extends Number>{
     //metodo abstracto
-    public void calculatesFee(T clubId);
+    public void calculateFees(T clubId);
 }
