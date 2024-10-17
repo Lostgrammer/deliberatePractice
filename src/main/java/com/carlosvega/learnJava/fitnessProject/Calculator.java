401a62efda913a7ab9b137e5a89702618cd7c36b
Carlos Vega <carlos_cmvp@hotmail.com>
2024-10-17 11:53:27 -0600

    set generic interface with a abstract method to calculate fee

--- src/main/java/com/carlosvega/learnJava/fitnessProject/Calculator.java
+++ src/main/java/com/carlosvega/learnJava/fitnessProject/Calculator.java
@@ -1,4 +1,6 @@
 package com.carlosvega.learnJava.fitnessProject;
-
-public interface Calculator {
+//crear una interfaz generica es como crear una clase
+public interface Calculator <T extends Number>{
+    //metodo abstracto
+    public void calculatesFee(T clubId);
 }
