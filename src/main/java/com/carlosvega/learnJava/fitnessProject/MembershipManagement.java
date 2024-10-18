bdfad0d003d906b998049b50f3051f302efb49c8
Carlos Vega <carlos_cmvp@hotmail.com>
2024-10-18 13:02:42 -0600

    set MemberShipManagement class with his methods

--- src/main/java/com/carlosvega/learnJava/fitnessProject/MembershipManagement.java
+++ src/main/java/com/carlosvega/learnJava/fitnessProject/MembershipManagement.java
@@ -7,8 +7,6 @@ import java.util.Scanner;
 //maneja el proceso de adicionar y eliminar miembros
 public class MembershipManagement {
     final private Scanner input = new Scanner(System.in);
-    LinkedList<Member> memberLinkedList = new LinkedList<>();
-    final private int multiClubValue = 4;
 
     private int getIntInput(){
         int choice = 0;
@@ -61,54 +59,89 @@ public class MembershipManagement {
         return choice;
     }
     private int setMemberID(LinkedList<Member> m){
-        if (m.size() > 0){
+        if (!m.isEmpty()){
             return m.getLast().getMemberId() + 1;
         }else {
             return 1;
         }
     }
     public String addMembers(LinkedList<Member> memberList){
+        int multiClubValue = 4; // se coloca aca porque solo se usara en este metodo
         String memShipName;
-        int memShipclub;
+        int memShipClub;
         String mem;
-        double memShipfees;
+        double memShipFees;
         int memShipMemberId;
         Member mbr;
-        Calculator<Integer> memShipNameCal; //estamos declarando una referencia de la interfaz
+        Calculator<Integer> memShipNameCal; //estamos declarando una referencia de la interfaz, solo se aceptaran valores integer
 
         System.out.println("Insert name: ");
         memShipName = input.nextLine();
         printClubOptions();
         System.out.println("Please enter club id");
-        memShipclub = getIntInput();
+        memShipClub = getIntInput();
         memShipMemberId = setMemberID(memberList);
         //calcular valor memShipNameCal
         //en caso el valor ingresado sea cualquiera menos la opcion multiclub
-        if(memShipclub != multiClubValue){
+
+        if(memShipClub != multiClubValue){
             //n seria el valor del argumento que reemplaza el parametro definido en la interfaz
             memShipNameCal = (n)->{
                 return switch (n){
-                    case 1 ->
-                }
-
+                    case 1 -> 900;
+                    case 2 -> 950;
+                    case 3 -> 1000;
+                    default -> 1;
+                };
             };
-            //mbr = new SingleClubMember(memShipMemberId,memShipMemberId,memShipName,);
+            memShipFees = memShipNameCal.calculateFees(memShipClub);//implementando la funcion lambda para calcular el valor que generara memShipNameCal y se le asginara a memShipFees
+            mbr = new SingleClubMember('S',memShipMemberId,memShipName, memShipFees, memShipClub);
+            System.out.println("Single Club Member added");
         //si se escogio multiclub
         }else{
-            //mbr = new MultiClubMember(memShipMemberId,memShipMemberId,memShipName,);
+            memShipNameCal = (n)-> {
+                return switch (n){
+                    case 4 -> 1200;
+                    default -> -1;
+                };
+            };
+            memShipFees = memShipNameCal.calculateFees(memShipClub);
+            mbr = new MultiClubMember('M',memShipMemberId,memShipName, memShipFees, 100);
+            System.out.println("Multi Club Member added");
         }
-
-
-        return memberList.toString();
+        mem = mbr.toString();
+        return mem;
     }
-    public void removeMembers(){
-
+    public void removeMember(LinkedList<Member> m){
+        int memberId = 0;
+        System.out.println("Enter member id you want to remove");
+        memberId = input.nextInt();
+        input.nextLine();
+        for (int i = 0; i < m.size(); i++) {
+            if (m.get(i).getMemberId() == memberId){
+                m.remove(i);
+                System.out.println("The user has been deleted");
+                return;
+            }
+        }
+        System.out.println("There is no user with id" + memberId);
     }
     public void quitProgram(){
-
+        System.out.println("Tnank you for use my program");
     }
 
-    public void showMemberInfo(){
-
+    public void showMemberInfo(LinkedList<Member> m){
+        int memberId = 0;
+        System.out.println("Enter member id you want to see info");
+        memberId = input.nextInt();
+        input.nextLine();
+        for (int i = 0; i < m.size(); i++) {
+            if (m.get(i).getMemberId() == memberId){
+                String[] memberInfo = m.get(i).toString().split(", ");
+                System.out.println(memberInfo);
+                return;
+            }
+        }
+        System.out.println("There is no user with id" + memberId);
     }
 }
