4c7c57af10ace5ad8d519c70e349a0a7e6acfcf2
Lostgrammer <carlos_cmvp@hotmail.com>
2024-10-17 21:35:51 -0600

    set interface as functional

--- src/main/java/com/carlosvega/learnJava/fitnessProject/Calculator.java
+++ src/main/java/com/carlosvega/learnJava/fitnessProject/Calculator.java
@@ -1,5 +1,6 @@
 package com.carlosvega.learnJava.fitnessProject;
 //crear una interfaz generica es como crear una clase
+@FunctionalInterface
 public interface Calculator <T extends Number>{
     //metodo abstracto
     public void calculateFees(T clubId);
